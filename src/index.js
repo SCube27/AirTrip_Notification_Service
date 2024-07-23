9b39288b5aaae3a2ac1f5bcd5cd7fd7b46bb257c
Sahil Shah <sahilshah2104@gmail.com>
2024-07-23 15:06:20 +0530

    Feat: Created Node Mailer Transport and Tested

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
